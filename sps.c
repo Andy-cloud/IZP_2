238cf4af87bf11100b1d2f10d02a2f8fe25a7a07
Andy-cloud <darmopilondra@gmail.com>
2020-11-27 13:38:00 +0100

    pokus_2

---
+++ sps.c
@@ -0,0 +1,14 @@
+/**
+ * file: sps.c
+ * author: xdarmo03
+ * date: 2020-11-27
+ * description: 
+ */ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const **argv)
+{
+    /* code */
+    return 0;
+}
